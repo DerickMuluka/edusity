e8bbcf3db754225687d1a33266e99a45894d6081
Derick Muluka <mulukaderick@gmail.com>
2024-06-26 12:36:19 +0300

    add About section

---
+++ src/components/About/About.jsx
@@ -0,0 +1,32 @@
+import React from 'react'
+import './About.css'
+import about_img from '../../assets/about.png'
+import play_icon from '../../assets/play-icon.png'
+
+const About = () => {
+  return (
+    <div className='about'>
+        <div className="about-left">
+            <img src={about_img} alt="" className='about-img' />
+            <img src={play_icon} alt="" className='play-icon' />
+        </div>
+        <div className="about-right">
+            <h3>ABOUT UNIVERSITY</h3>
+            <h2>Nurturing Tomorrow's Leaders Today</h2>
+            <p>Embark on a transformative educational journey with our university's education programs. Our
+                cutting-edge is designed to empower students with knowledge, skills and experiences needed to excel
+                in the dynamic field of education.
+            </p>
+            <p>With a focus on innovation, hands-on learning and personalized mentorship, our programs prepare
+                aspiring educators to make a meaningful impacts in classrooms, school and communities.
+            </p>
+            <p>Whether you inspire to become a teacher, administrator, counselor or educational leader, our diverse chnage
+                of programs offers the perfrect pathway to achieve your goals and unlock your full potential in shaping the 
+                future of education.
+            </p>
+        </div>
+    </div>
+  )
+}
+
+export default About
